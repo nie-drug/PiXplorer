a90dccbbd2f21f3d5ac301237805999d87543958
Neil A <76754251+nie-drug@users.noreply.github.com>
2022-11-08 20:59:03 +0400

    Update MainActivity.java

--- app/src/main/java/com/projectmaterial/myapplication/MainActivity.java
+++ app/src/main/java/com/projectmaterial/myapplication/MainActivity.java
@@ -2,13 +2,13 @@ package com.projectmaterial.myapplication;
 
 import android.os.Bundle;
 
-import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends CollapsingToolbarBaseActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
     }
 }

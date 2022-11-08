e7038ebf5d76c4ba55e38fd9f39c6b85e3886665
Neil A <76754251+nie-drug@users.noreply.github.com>
2022-11-08 04:16:47 +0400

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application") version "7.3.1"
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+android {
+    compileSdkVersion(32)
+    defaultConfig {
+        applicationId = "com.projectmaterial.myapplication"
+        minSdkVersion(23)
+        targetSdkVersion(32)
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.preference:preference:1.2.0")
+}
